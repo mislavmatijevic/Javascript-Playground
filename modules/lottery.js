02442dd2b9b047b033ac89281b3227dfec5e11f7
Seljumnik <bixi36@gmail.com>
2021-04-09 19:36:43 +0200

    Complete lottery commit

--- modules/lottery.js
+++ modules/lottery.js
@@ -11,8 +11,9 @@ export default class Lottery {
     constructor(people) {
         this.people = people;
         this.players = [];
-        this.WinningCombination = [];
+        this.winningCombination = [];
     }
+
     getLotteryNumbers() {
         let lotteryNumbers = [];
         while (lotteryNumbers.length < 4)
@@ -20,13 +21,38 @@ export default class Lottery {
         lotteryNumbers.sort();
         return lotteryNumbers;
     }
+
     generatePlayers() {
-        for (person in people)
+        for (person in this.people)
         {
-            players.push(new Player(person.name, person.surname, getLotteryNumbers()));
+            this.players.push(new Player(person.name, person.surname, getLotteryNumbers()));
         }
     }
-    getWinningCombination() {
 
+    getwinningCombination() {
+        this.winningCombination = this.getLotteryNumbers();
+    }
+
+    startDrawing() {
+        this.generatePlayers();
+        this.getwinningCombination();
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const winners = this.players
+                    .filter(player => player.lotteryNumbers
+                        .every((val, index) => val === this.winningCombination[index]));
+                const result = {
+                    winningCombination: this.winningCombination,
+                    winners
+                };
+
+                if (winners.length > 0) {
+                    resolve(result);
+                } else {
+                    reject(result);
+                }
+            }, 2000);
+        });
     }
 }
