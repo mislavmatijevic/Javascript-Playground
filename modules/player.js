632c4a86d5bbb517a087f05cfaee12706c054c76
Seljumnik <bixi36@gmail.com>
2021-04-09 18:46:03 +0200

    first commit

---
+++ modules/player.js
@@ -0,0 +1,8 @@
+export class Player {
+    constructor(name, surname, lotteryNumbers)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.lotteryNumbers = lotteryNumbers;
+    }
+}
